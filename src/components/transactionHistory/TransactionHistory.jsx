c9a505cb61cd64525b54325ffc9d419f04f0e2b0
AllaYakovenko <jaak280488@gmail.com>
2023-01-16 21:24:01 +0200

    add style

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -4,20 +4,20 @@ import css from 'components/transactionHistory/TransactionHistory.module.css';
 const TransactionHistory = ({ items }) => {
     return (
         <table className={css.transactionHistory}>
-            <thead>
+            <thead className={css.transactionProperty}>
                 <tr>
-                    <th>Type</th>
-                    <th>Amount</th>
-                    <th>Currency</th>
+                    <th className={css.transactionType}>Type</th>
+                    <th className={css.transactionAmount}>Amount</th>
+                    <th className={css.transactionCurrency}>Currency</th>
                 </tr>
             </thead>
 
             <tbody>
                 {items.map(item => (
                     <tr key={item.id}>
-                        <td>{item.type}</td>
-                        <td>{item.amount}</td>
-                        <td>{item.currency}</td>
+                        <td className={css.dataType}>{item.type}</td>
+                        <td className={css.dataAmount}>{item.amount}</td>
+                        <td className={css.dataCurrency}>{item.currency}</td>
                     </tr>
                 ))}
             </tbody>
