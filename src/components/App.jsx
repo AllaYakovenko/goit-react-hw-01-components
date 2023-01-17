81deb87caea3c5c65cb515cfb98ac2d02e9d3f3c
AllaYakovenko <jaak280488@gmail.com>
2023-01-17 17:40:52 +0200

    add style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,7 +28,7 @@ export default function App(){
       />
 
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      {/* <Statistics stats={data} /> */}
 
       <FriendList friends={friends} />
 
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -2,18 +2,7 @@ import PropTypes from 'prop-types';
 import css from 'components/statistics/Statistics.module.css';
 
 function getRandomHexColor() { 
-  // let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  // return bgColor;
-
-let color =
-    'rgb(' +
-    Math.round(Math.random() * 255) +
-    ',' +
-    Math.round(Math.random() * 255) +
-    ',' +
-    Math.round(Math.random() * 255) +
-    ')';
-
+  let color = `#${Math.floor(Math.random() * 12345678).toString(16)}`;
   return color;
 }
 
@@ -26,7 +15,7 @@ const Statistics = ({ title, stats }) => {
         {stats.map(stat => (
           <li className={css.item}
             style={{
-              backroundColor: getRandomHexColor(),
+              backgroundColor: getRandomHexColor(),
             }}
             key={stat.id}
           >
